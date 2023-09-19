5333a032c1993eabccebb101f169fae430d48469
cipp00h <12seokeun@gmail.com>
2023-09-19 16:33:36 +0900

    build: 프로젝트 초기 개발 환경 셋팅 #2
    - Spring Boot 프로젝트 초기 개발 환경 셋팅
     * Project: Gradle - Groovy
     * Language: JAVA
     * Spring Boot Version: 3.1.3
     * JAVA Version: 17
     * Dependencies:
      ** Lombok
      ** Spring Web
      ** Spring Security
      ** H2 Database
      ** MySQL Driver
      ** Spring Boot DevTools
      ** netty-socketio:1.7.23
      ** jjwt:0.9.1
    
    Closes #2

---
+++ src/test/java/com/ssap/SSAPIDE/SsapWebIdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssap.SSAPIDE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsapWebIdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
