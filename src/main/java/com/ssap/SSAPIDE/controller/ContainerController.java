6b399f7668b59d9a39c59149e1d647de73ef8d2d
cipp00h <12seokeun@gmail.com>
2023-09-27 01:41:15 +0900

    새로운 커밋 메시지

---
+++ src/main/java/com/ssap/SSAPIDE/controller/ContainerController.java
@@ -0,0 +1,48 @@
+package com.ssap.SSAPIDE.controller;
+
+import com.ssap.SSAPIDE.dto.ContainerRequestDto;
+import com.ssap.SSAPIDE.dto.ContainerResponseDto;
+import com.ssap.SSAPIDE.service.ContainerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/containers")
+    public class ContainerController {
+    @Autowired
+    private ContainerService containerService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createContainer(@RequestBody ContainerRequestDto requestDto) {
+        try {
+            ContainerResponseDto container = containerService.createContainer(
+                    requestDto.getTitle(),
+                    requestDto.getDescription(),
+                    requestDto.getStack(),
+                    requestDto.getCustomControl());
+            log.info("Container created with ID: {}", container.getContainerId());
+            return ResponseEntity.ok().body(
+                    new ContainerResponseDto(container.getContainerId(), container.getCreatedAt()));
+        } catch (Exception e) {
+            log.error("Error while creating container", e);
+            return ResponseEntity.status(500).body("요청을 처리하는 중에 서버에서 오류가 발생했습니다.");
+        }
+    }
+
+    @PostMapping("/run/{containerId}")
+    public ResponseEntity<?> runContainer(@PathVariable String containerId) {
+        try {
+            containerService.runContainer(containerId);
+            return ResponseEntity.ok().body(containerId);
+        } catch (Exception e) {
+            log.error("Error while starting container", e);
+            return ResponseEntity.status(500).body("요청을 처리하는 중에 서버에서 오류가 발생했습니다.");
+        }
+    }
+
+
+}
+
