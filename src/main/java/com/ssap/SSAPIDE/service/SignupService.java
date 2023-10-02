058847f4794244b03bfcb7faed31b55efce827d5
CaffeineTurtle <sungnui@naver.com>
2023-10-03 06:52:44 +0900

    feature: 회원가입 기능 개발 #5
    - SignupService 코드 정리

--- src/main/java/com/ssap/SSAPIDE/service/SignupService.java
+++ src/main/java/com/ssap/SSAPIDE/service/SignupService.java
@@ -4,9 +4,11 @@ import com.ssap.SSAPIDE.domain.member.User;
 import com.ssap.SSAPIDE.dto.SignupRequest;
 import com.ssap.SSAPIDE.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SignupService {
